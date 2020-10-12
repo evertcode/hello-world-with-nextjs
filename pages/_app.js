e1e22b3c5b86f6e1319c06612af424fef94ad67a
Hebert Hernández <hebert.hernandez7@gmail.com>
2020-10-12 06:20:50 -0500

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import "../styles/index.css";
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0,width=device-width" />
+        <title>Hello world</title>
+        <meta
+          key="description"
+          name="description"
+          content="Example Hello world with netxjs"
+        />
+      </Head>
+      <Component />
+    </>
+  );
+}
